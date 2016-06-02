337ced0bbeddba969d55e82da98f74dd4a270eb7
dungdb1 <badungbkhn@gmail.com>
2016-06-02 11:20:33 +0700

    first commit

---
+++ src/viettel/com/rtc_git/git.java
@@ -0,0 +1,14 @@
+package viettel.com.rtc_git;
+
+public class git {
+
+	public git() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
